ed20c114f2484ea2a201bb1bca4fe54fc184cd78
alessandrogammieri <alessandrogammieri.it@gmail.com>
2019-07-15 15:32:24 +0200

    sex color

--- sexcolor/script.js
+++ sexcolor/script.js
@@ -4,5 +4,31 @@ Sulla pagina html deve apparire “Ciao <nome>“, il colore del nome deve esser
 azzurro o rosa a seconda del sesso inserito. */
 
 
+// Dichiarazione delle variabili
+var name, element, sesso, domanda;
 
-console.log("Ciao a tutti");
+// Chiediamo all'utente il suo nome
+name = prompt("Inserisci qui il tuo nome");
+
+// Assegnamo ad una variabile il nostro id ed element è un DOM Object Element
+element = document.getElementById('mio_id');
+
+// Creiamo una variabile di appoggio
+sesso = false;
+
+// Ciclo while per determinare il colore
+while (sesso == false) {
+  domanda = prompt("Specifica il tu sesso, scrivi Maschio o Femmina");
+  if (domanda == "Maschio") {
+    element.style.color = "blue";
+    sesso = true;
+  } else if (domanda == "Femmina") {
+    element.style.color = "pink";
+    sesso = true;
+  } else {
+    alert('Richiesta errata. Riprova!');
+  }
+}
+
+// Stampiamo il nome dell'utente
+document.getElementById('mio_id').innerHTML = "Ciao " + name;
