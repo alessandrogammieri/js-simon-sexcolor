80f62f7ce2b927bff40f25053cd79132547950a7
alessandrogammieri <alessandrogammieri.it@gmail.com>
2019-07-15 18:39:07 +0200

    simon

--- simon/script.js
+++ simon/script.js
@@ -4,4 +4,76 @@ precedentemente.
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei
 numeri da indovinare sono stati individuati; */
 
-console.log("Ciao a tutti");
+// Dichiarazione delle variabili
+var number, list, start, clock, memo, scelta, trovato = false;
+
+// Lista contenente i 5 numeri
+list = [];
+console.log(list);
+scelta = [];
+console.log(scelta);
+
+
+// Generiamo 5 numeri casuali
+for (var i = 0; i < 5; i++) {
+  number = Math.floor(Math.random() * 101);
+  list.push(number);
+}
+
+// L'alert espone i 5 numeri
+alert("Memorizza i 5 numeri e premi OK: " + list );
+
+// Conto alla rovescia di 30 secondi
+start = 30;
+
+clock = setInterval(countdown, 1000);
+
+function countdown () {
+  if (start == 0) {
+    alert("Sei pronto!");
+    clearInterval(clock)
+    // Funzione per i prompt di inserimento numeri
+    function insertNum () {
+      memo = parseInt(prompt("Inserisci il numero che hai memorizzato"));
+      return memo;
+    }
+    // Richiedo per 5 volte di inserire il numero
+    for (var i = 0; i < 5; i++) {
+      insertNum ()
+      if (list[i] == memo) {
+        trovato = true;
+        scelta.push(memo);
+      }
+    }
+    document.getElementById('risultato').innerHTML = "I numeri indovinati sono " + scelta.length + ": " + scelta;
+    if (scelta.length === 0) {
+      document.getElementById('risultato').innerHTML = "Nessun numero indovinato";
+    }
+  } else {
+    start--;
+    document.getElementById('mio_id').innerHTML = "Inizia il conto alla rovescia " + start;
+  }
+}
+
+// Anonymous functions - per provare funzionalità
+clock = setInterval(function() {
+  if (start == 0) {
+    alert("Sei pronto!");
+    clearInterval(clock)
+    // Richiedo per 5 volte di inserire il numero
+    for (var i = 0; i < 5; i++) {
+      memo = parseInt(prompt("Inserisci il numero che hai memorizzato"));
+      if (list[i] == memo) {
+        trovato = true;
+        scelta.push(memo);
+      }
+    }
+    document.getElementById('risultato').innerHTML = "I numeri indovinati sono " + scelta.length + ": " + scelta;
+    if (scelta.length === 0) {
+      document.getElementById('risultato').innerHTML = "Nessun numero indovinato";
+    }
+  } else {
+    start--;
+    document.getElementById('mio_id').innerHTML = "Inizia il conto alla rovescia " + start;
+  }
+}, 1000);
